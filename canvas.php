d484b4a6ecef8b4beae4ae47709b5e02cd33dd94
Matheus Nene <matheusvicente27@gmail.com>
2018-10-26 17:07:58 -0300

    qualquer coisa

--- canvas.php
+++ canvas.php
@@ -7,6 +7,9 @@
 	<script type="text/javascript" src="javascript/jquery-3.3.1.min.js"></script>
 </head>
 <body id="body">
+	<audio autoplay="true" controls="controls">
+		<source src="" type="">
+	</audio>
 	<div class="titulo">
 		<h1 class="title">Bem-vindo ao F.O.D.E.U. Projeto</h1>
 		<h2 class="title">Fight Of Destructive Endless Universe</h2>
@@ -38,8 +41,18 @@
 	<img src="assets/img/BonecoDir02.png" style="display: none" id="zikudaDir01" height="150">
 	<img src="assets/img/BonecoEsq03.png" style="display: none" id="zikudaEsq02" height="150">
 	<img src="assets/img/BonecoDir03.png" style="display: none" id="zikudaDir02" height="150">
-	
-
+	<img src="assets/img/BonecoEsq04.png" style="display: none" id="zikudaEsq03" height="150">
+	<img src="assets/img/BonecoDir04.png" style="display: none" id="zikudaDir03" height="150">
+	<img src="assets/img/inimigoTerrestre01.png" style="display: none" id="inimigoTerrestre01" height="150">
+	<img src="assets/img/inimigoTerrestre02.png" style="display: none" id="inimigoTerrestre02" height="150">
+	<img src="assets/img/inimigoTerrestre03.png" style="display: none" id="inimigoTerrestre03" height="150">
+	<img src="assets/img/inimigoTerrestre04.png" style="display: none" id="inimigoTerrestre04" height="150">
+	<img src="assets/img/inimigoTerrestre05.png" style="display: none" id="inimigoTerrestre05" height="150">
+	<img src="assets/img/inimigoTerrestreDir01.png" style="display: none" id="inimigoTerrestreDir01" height="150">
+	<img src="assets/img/inimigoTerrestreDir02.png" style="display: none" id="inimigoTerrestreDir02" height="150">
+	<img src="assets/img/inimigoTerrestreDir03.png" style="display: none" id="inimigoTerrestreDir03" height="150">
+	<img src="assets/img/inimigoTerrestreDir04.png" style="display: none" id="inimigoTerrestreDir04" height="150">
+	<img src="assets/img/inimigoTerrestreDir05.png" style="display: none" id="inimigoTerrestreDir05" height="150">
 
 </body>
 </html>
--- javascript/script.js
+++ javascript/script.js
@@ -59,11 +59,11 @@ var draw = () => {
 
 	function keyDown(e){
 
-		if(e.keyCode == 39){
+		if(e.keyCode == 39 || e.keyCode == 68 || e.keyCode == 100){
 		
 			virarPraDireita();
 		
-		} else if(e.keyCode == 37) {
+		} else if(e.keyCode == 37 || e.keyCode == 65 || e.keyCode == 97) {
 		
 			virarPraEsquerda();
 		
@@ -115,24 +115,44 @@ var draw = () => {
 
 						ctx.clearRect(0,0,680,400);
 						ctx.beginPath();
-						img = document.getElementById('zikudaEsq01');
-						ctx.drawImage(img, 270, 180);
+						img = document.getElementById('zikudaEsq03');
+						ctx.drawImage(img, 248, 173);
 
-						var intAtaqEsq02 = setInterval(function(){
+						var vamosVoltar = setInterval(function(){
 
 							ctx.clearRect(0,0,680,400);
 							ctx.beginPath();
-							img = document.getElementById('zikudaEsq');
-							ctx.drawImage(img,270,180);
-							clearInterval(intAtaqEsq02);
+							img = document.getElementById('zikudaEsq02');
+							ctx.drawImage(img, 248,173);							
 
-						}, 50);
+							var intAtaqEsq02 = setInterval(function(){
+
+								ctx.clearRect(0,0,680,400);
+								ctx.beginPath();
+								img = document.getElementById('zikudaEsq01');
+								ctx.drawImage(img, 270, 180);
+
+								var intAtaqEsq03 = setInterval(function(){
+
+									ctx.clearRect(0,0,680,400);
+									ctx.beginPath();
+									img = document.getElementById('zikudaEsq');
+									ctx.drawImage(img,270,180);
+									clearInterval(intAtaqEsq03);
+
+								}, 40);
+								clearInterval(intAtaqEsq02);
+
+							}, 40);
+							clearInterval(vamosVoltar);
+
+						}, 40);
 						clearInterval(intAtaqEsq01);
 
-					}, 50);
+					}, 40);
 
 					clearInterval(intAtaqEsq);
-				}, 50);
+				}, 40);
 
 			}else if(lado == '0'){
 
@@ -145,35 +165,243 @@ var draw = () => {
 					ctx.clearRect(0,0,680,400);
 					ctx.beginPath();
 					img = document.getElementById('zikudaDir02');
-					ctx.drawImage(img, 270, 172);
+					ctx.drawImage(img, 270,173);
 
 					var intAtaqDir01 = setInterval(function(){
 
 						ctx.clearRect(0,0,680,400);
 						ctx.beginPath();
-						img = document.getElementById('zikudaDir01');
-						ctx.drawImage(img, 270, 180);
+						img = document.getElementById('zikudaDir03');
+						ctx.drawImage(img, 270, 173);
 
-						var intAtaqDir02 = setInterval(function(){
+						var vamosVoltar = setInterval(function(){
 
 							ctx.clearRect(0,0,680,400);
 							ctx.beginPath();
-							img = document.getElementById('zikudaDir');
-							ctx.drawImage(img,270,180);
-							clearInterval(intAtaqDir02);
+							img = document.getElementById('zikudaDir02');
+							ctx.drawImage(img, 270,173);							
 
-						}, 50);
+							var intAtaqDir02 = setInterval(function(){
+
+								ctx.clearRect(0,0,680,400);
+								ctx.beginPath();
+								img = document.getElementById('zikudaDir01');
+								ctx.drawImage(img, 270, 180);
+
+								var intAtaqDir03 = setInterval(function(){
+
+									ctx.clearRect(0,0,680,400);
+									ctx.beginPath();
+									img = document.getElementById('zikudaDir');
+									ctx.drawImage(img,270,180);
+									clearInterval(intAtaqDir03);
+
+								}, 40);
+								clearInterval(intAtaqDir02);
+
+							}, 40);
+							clearInterval(vamosVoltar);
+
+						}, 40);
 						clearInterval(intAtaqDir01);
 
-					}, 50);
+					}, 40);
 
 					clearInterval(intAtaqDir);
-				}, 50);
-
+				}, 40);
 			}
 		}
 	}
 
 	virarPraEsquerda();
 
+	var aparecerInimigo = () => {
+
+		ctx.beginPath();
+		img = document.getElementById('inimigoTerrestre01');
+		ctx.drawImage(img,110,170);
+	
+	}
+	var aparecerInimigo1 = () => {
+
+		ctx.beginPath();
+		img = document.getElementById('inimigoTerrestre02');
+		ctx.drawImage(img,-10,155);
+	
+	}
+	var aparecerInimigo2 = () => {
+
+		ctx.beginPath();
+		img = document.getElementById('inimigoTerrestre03');
+		ctx.drawImage(img,-70,160);
+	
+	}
+	var aparecerInimigo3 = () => {
+
+		ctx.beginPath();
+		img = document.getElementById('inimigoTerrestre04');
+		ctx.drawImage(img,110,175);
+	
+	}
+	var aparecerInimigo4 = () => {
+
+		ctx.beginPath();
+		img = document.getElementById('inimigoTerrestre05');
+		ctx.drawImage(img,20,175);
+	
+	}
+	var aparecerInimigoDir = () => {
+
+		ctx.beginPath();
+		img = document.getElementById('inimigoTerrestreDir01');
+		ctx.drawImage(img,300,170);
+	
+	}
+	var aparecerInimigoDir1 = () => {
+
+		ctx.beginPath();
+		img = document.getElementById('inimigoTerrestreDir02');
+		ctx.drawImage(img,350,155);
+	
+	}
+	var aparecerInimigoDir2 = () => {
+
+		ctx.beginPath();
+		img = document.getElementById('inimigoTerrestreDir03');
+		ctx.drawImage(img,460,160);
+	
+	}
+
+	// aparecerInimigo();
+	// aparecerInimigo1();
+	// aparecerInimigo2();
+	// aparecerInimigo3();
+	// aparecerInimigo4();
+	// aparecerInimigoDir();
+	// aparecerInimigoDir1();
+	// aparecerInimigoDir2();
+
+	var i = -150;
+	var j = 1;
+	var altE = 0;
+	
+	let enviaInimigoEsq = () =>{
+		let enviaInimigoEsqVar = setInterval(function(){
+
+			if(i!=130){
+				ctx.beginPath();
+				ctx.clearRect(0,0,300,400);
+				img = document.getElementById('inimigoTerrestre0'+j);
+
+				if(l==1)
+					altE = 170;
+				else if(l==2)
+					altE = 160;
+				else if(l==3)
+					altE = 155;
+				else if(l==4 || l==5)
+					altE = 175;
+
+				
+				ctx.drawImage(img,i,altE);
+				i++;
+
+				if(i%50==0)
+					j=1;
+				else if(i%10==0)
+					j++;
+
+
+				// if(i==130)
+					// alert('vc perdeu');
+
+			}else{
+				clearInterval(enviaInimigoEsqVar);
+			}
+		},10);
+	} 
+
+	var k = 600;
+	var l = 1;
+	var altD = 0;
+
+	let enviaInimigoDir = () =>{
+		let enviaInimigoDirVar = setInterval(function(){
+
+			if(k!=270){
+				ctx.beginPath();
+				ctx.clearRect(400,0,700,400);
+				img = document.getElementById('inimigoTerrestreDir0'+l);
+
+				if(l==1)
+					altD = 170;
+				else if(l==2)
+					altD = 160;
+				else if(l==3)
+					altD = 155;
+				else if(l==4 || l==5)
+					altD = 175;
+
+				ctx.drawImage(img,k,altD);
+
+				k--;
+
+				if(k%50==0)
+					l=1;
+				else if(k%10==0)
+					l++;
+
+
+				// if(k==270)
+					// alert('vc perdeu');
+
+			}else{
+				clearInterval(enviaInimigoDirVar);
+			}
+		},10);
+	}	
+
+
+
+	function getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
+
+	for(var algumaCoisa = 0; algumaCoisa < 10; algumaCoisa++)
+		setTimeout(criarUnico, 1000);
+
+	for(var outraCoisa = 0; outraCoisa < 10; outraCoisa++){
+		if(sorteados[outraCoisa] % 2 == 0){
+			// var enviaInimigosEsq = setInterval(function(){
+			// 	enviaInimigoEsq();
+			// 	clearInterval(enviaInimigosEsq);
+			// },1000);	
+			setTimeout(enviaInimigoEsq, 1000);
+		} else {
+			// var enviaInimigosDir = setInterval(function(){
+			// 	enviaInimigoDir();
+			// 	clearInterval(enviaInimigosDir);
+			// },1000);
+			setTimeout(enviaInimigoDir, 1000);
+		}
+	}
 }
+
+	var sorteados = [];
+	var valorMaximo = 1000;
+
+	function criarUnico() {
+	    if (sorteados.length == valorMaximo) {
+	        if (confirm('Já não há mais! Quer recomeçar?')) sorteados = [];
+	        else return;
+	    }
+	    var sugestao = Math.ceil(Math.random() * valorMaximo); // Escolher um numero ao acaso
+	    while (sorteados.indexOf(sugestao) >= 0) {  // Enquanto o numero já existir, escolher outro
+	        sugestao = Math.ceil(Math.random() * valorMaximo);
+	    }
+	    sorteados.push(sugestao); // adicionar este numero à array de numeros sorteados para futura referência
+	    console.log(sorteados);
+	    return sugestao; // devolver o numero único
+	}
